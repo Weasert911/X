9beba4d52abe1dae58bccc9469a19bc06dc261f6
Weasert911 <rk13579weasert@gmail.com>
2026-04-21 23:25:45 +0530

    .

--- GAME/game-files/script/postprocess/auto_shader_applier.gd
+++ GAME/game-files/script/postprocess/auto_shader_applier.gd
@@ -3,22 +3,22 @@ extends Node3D
 @export var shader: Shader
 
 func _ready():
-    if shader == null:
-        push_error("No shader assigned!")
-        return
+	if shader == null:
+		push_error("No shader assigned!")
+		return
 
-    apply_shader_recursive(self)
+	apply_shader_recursive(self)
 
 
 func apply_shader_recursive(node: Node):
-    for child in node.get_children():
+	for child in node.get_children():
 
-        # If it's a mesh
-        if child is MeshInstance3D:
-            var mat := ShaderMaterial.new()
-            mat.shader = shader
+		# If it's a mesh
+		if child is MeshInstance3D:
+			var mat := ShaderMaterial.new()
+			mat.shader = shader
 
-            child.material_override = mat
+			child.material_override = mat
 
-        # Recurse
-        apply_shader_recursive(child)
+		# Recurse
+		apply_shader_recursive(child)
